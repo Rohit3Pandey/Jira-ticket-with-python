c51f3d0870638e277668f9af05fe00d84471b018
Rohit Pandey <153757342+Rohit3Pandey@users.noreply.github.com>
2024-03-25 23:53:16 +0530

    Update git-jira.py

--- git-jira.py
+++ git-jira.py
@@ -1,10 +1,67 @@
 from flask import Flask
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 app = ("__name__")
 
-@app.route("/")
+@app.route("/CreateJira", methods=["POST"])
 
-def hello():
-    return "hello world"
+def CreateJira():
+    url = "https://rohitpandey-jira.atlassian.net/rest/api/3/issue"
 
-app.run("0.0.0.0")
+    API_TOKEN = ""
+    
+    auth = HTTPBasicAuth("@gmail.com", API_TOKEN)
+    
+    headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+    
+    payload = json.dumps( {
+      "fields": {
+        
+        "description": {
+          "content": [
+            {
+              "content": [
+                {
+                  "text": "My first Jira-ticket",
+                  "type": "text"
+                }
+              ],
+              "type": "paragraph"
+            }
+          ],
+          "type": "doc",
+          "version": 1
+        },
+        
+        "issuetype": {
+          "id": "10014"
+        },
+    
+        "project": {
+            "key": "RP"
+        },
+        "summary": "Main order flow broken",
+        
+      }
+    })   
+        
+    
+    response = requests.request(
+       "POST",
+       url,
+       data=payload,
+       headers=headers,
+       auth=auth
+    )
+    
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+
+    
+
+
+app.run("0.0.0.0", PORT = 5000)
